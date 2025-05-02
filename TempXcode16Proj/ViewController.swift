15cf48e7a2fb39672d6c17462a4d245ea81042a2
Aleksey Berezka <a.berezka@dodobrands.io>
2025-05-02 06:53:03 +0300

    Folders

---
+++ TempXcode16Proj/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TempXcode16Proj
+//
+//  Created by Aleksey Berezka on 01.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonTapped(_ sender: Any) {
+        label.text = "Button tapped at \(Date().formatted(date: .omitted, time: .complete))"
+    }
+    
+}
+
