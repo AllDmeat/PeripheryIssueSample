0e87e4474f64589fab41747f39a93725a8c898cc
Aleksey Berezka <alldmeat@icloud.com>
2026-02-13 12:17:39 +0500

    Integrate button from DS into main project

---
+++ MyProject/ClearButtonView.swift
@@ -0,0 +1,16 @@
+//
+//  ClearButtonView.swift
+//  MyProject
+//
+//  Created on 13.02.2026.
+//
+
+import SwiftUI
+import SampleDS
+
+struct ClearButtonView: View {
+    var body: some View {
+        Button("Clear") { }
+            .buttonStyle(.clear)
+    }
+}
--- MyProject/ViewController.swift
+++ MyProject/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
     
@@ -27,6 +28,22 @@ class ViewController: UIViewController {
     
     @IBAction func confirmTapped() {
         print("This functions is actually used")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let hostingController = UIHostingController(rootView: ClearButtonView())
+        addChild(hostingController)
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(hostingController.view)
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        hostingController.didMove(toParent: self)
     }
 }
 
